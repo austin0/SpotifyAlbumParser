35ac7b07bd6a3563cfe28399bc06fae04d574022
Austin <zzirnathan@hotmail.com>
2019-05-18 01:36:55 +0100

    Improved usability and refactored code

--- SpotifyAlbumParser.py
+++ SpotifyAlbumParser.py
@@ -7,50 +7,55 @@ import spotipy.util as util
 import json
 #import pprint
 
-artist_name = ""
-addAlbum = "y"
+addAlbum = True
 sqlString = ""
+userDecision = ""
 
-if len(sys.argv) > 1:
-    urn = sys.argv[1]
-else:
-    urn = 'spotify:album:2T6jeELx5BqH4GMLObBy10'
-
-username = "*/ENTER SPOTIFY USERNAME HERE/*" # your spotify username will be the email address you use to log in.
+username = "/*ENTER SPOTIFY USERNAME HERE*/" # your spotify username will be the email address you use to log in.
+client_id="/*ENTER CLIENT ID HERE*/" # your spotify client keys can be retrieved from the developer.spotify.com page
+client_secret="/*ENTER SECRET CLIENT KEY HERE*/"
 scope = "user-library-read"
+redirect_uri='http://localhost/'
 
-token = util.prompt_for_user_token(username,scope,client_id='*/ENTER  CLIENT ID HERE/*',client_secret='*/ENTER SECRET CLIENT KEY HERE/*',redirect_uri='http://localhost/') # your spotify client keys can be retrieved from the developer.spotify.com page
+token = util.prompt_for_user_token(username,scope,client_id,client_secret,redirect_uri)
 
-while addAlbum == "y":
+while addAlbum:
 
+    urn = (input("Enter a new spotify album URN:\n"))
     sp = spotipy.Spotify(auth=token)
     album = sp.album(urn)
-    #pprint.pprint(album) #pprint is used to format the json returned by the spotify api, enable this to view raw data
+    #pprint.pprint(album) # pprint is used to format the json returned by the spotify api, enable this to view raw data
     
-    newdump = json.dumps(album)
-    parsed_json = json.loads(newdump)
-    artistdump = json.dumps(parsed_json['artists'])
-    artist_json = json.loads(artistdump)
+    parsed_json = json.loads(json.dumps(album))
+    artist_json = json.loads(json.dumps(parsed_json['artists']))
     
     print(parsed_json['name'])
     print(parsed_json['album_type'])
     print(parsed_json['label'])
     print(parsed_json['release_date'])
     print(parsed_json['total_tracks'])
-    for i in artist_json:
-        artist_name = (i['name'])
-    print(parsed_json['genres'])
+    for artist in artist_json:
+        artist_name = (artist['name'])
+        print(artist_name)
+    print(parsed_json['genres']) # if genres is empty spotify has not classified the album yet
     
-    sqlString += ("INSERT INTO album(name, release_date, type, genre, track_count) VALUES('{}', '{}', '{}', '{}', {});\n".format(parsed_json['name'], parsed_json['release_date'], parsed_json['album_type'], parsed_json['genres'], parsed_json['total_tracks']))
+    sqlString += ("INSERT INTO album(name, release_date, type, genre, track_count) VALUES('{}', '{}', '{}', '{}', {});\n".format
+                    (parsed_json['name'],
+                     parsed_json['release_date'], 
+                     parsed_json['album_type'], 
+                     parsed_json['genres'], 
+                     parsed_json['total_tracks']))
+                     
     sqlString += ("INSERT INTO artist(name) VALUES('{}');\n".format(artist_name))
     
-    addAlbum = str(input("Add another album? y/n?\n"))
-    if addAlbum != "y":
-        print("Exiting...")
-        break
+    while userDecision not in {"y", "n"}:
+        userDecision = str(input("Add another album? y/n?\n")).lower().strip()
+        if userDecision == "n":
+            print("Exiting...")
+            addAlbum = False
     
-    urn = str(input("Enter a new spotify album URN:\n"))
+    userDecision = ""
     
-text_file = open("albums2.txt", "w")
+text_file = open("albums.txt", "w")
 text_file.write(sqlString)
 text_file.close()
